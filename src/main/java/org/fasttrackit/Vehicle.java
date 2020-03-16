05592ea1acc258175f68e6fe7a1b598c8c8032eb
CarlaClej <clej.carla@yahoo.ro>
2020-03-16 18:53:34 +0200

    added mileage multiplier

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -29,7 +29,10 @@ public class Vehicle {
             System.out.println("Valid speed entered.");
         }
 
-        // todo: use more fuel if speed > 120
+        double mileageMultiplier = 1;
+        if (speed > 120) {
+            mileageMultiplier = speed / 100;
+        }
 
         System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + " h ");
 
@@ -43,6 +46,8 @@ public class Vehicle {
         System.out.println("Total traveled distance: " + totalDistance);
 
         double usedFuel = traveledDistance * mileage / 100;
+
+        usedFuel *= mileageMultiplier;
 
         fuelLevel = fuelLevel - usedFuel;
 
